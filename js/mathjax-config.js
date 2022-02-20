41e7c3884627a60138070f6f39052bc375118570
fudong <13908171+solitongas@users.noreply.github.com>
2022-02-20 18:43:52 -0500

    update posts

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -5,6 +5,7 @@
 // - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
 window.MathJax = {
   tex: {
+    tags: 'ams',
     inlineMath: [
       ['$', '$'],
       ['\\(', '\\)'],
